e933ba787cc8d84ed7078d1626daa14348584917
rajsekhar240 <rajsekharmahapatro@gmail.com>
2025-08-06 23:38:53 +0400

    Created date modified date changes
    Added the created Date and last modified date fields to the entity, enabled auditing for this to work and added the entitylisteners with AuditingEntityListeners class

--- src/main/java/io/app/indiantourister/model/Customer.java
+++ src/main/java/io/app/indiantourister/model/Customer.java
@@ -3,7 +3,11 @@ package io.app.indiantourister.model;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Set;
 
 @Entity
@@ -13,6 +17,7 @@ import java.util.Set;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -22,6 +27,12 @@ public class Customer {
     private Long version;
     private String firstName;
     private String lastName;
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime lastModifiedDate;
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private Set<Address> address;
